626bef9b8f5237ac5bed22f6c9ab815132602c09
cpt.kokao <cpt.kokao@gmail.com>
2025-10-01 11:04:15 +0300

    [4] Добавляет компоненты постов и пагинацию

--- src/api/postsApi.js
+++ src/api/postsApi.js
@@ -1,13 +1,13 @@
 import baseApi from './baseApi';
 
-export const getPosts = (page) => baseApi.get(`/posts?_page=${page}&limit=10&_expand=user`);
+export const getPosts = (page) => baseApi.get(`/posts?_page=${page}&_limit=10&_embed=user`);
 
 export const getPostsByUser = (userId, page) =>
-	baseApi.get(`/posts?userId=${userId}&_page=${page}&limit=10&_expand=user`);
+	baseApi.get(`/posts?userId=${userId}&_page=${page}&_limit=10&_expand=user`);
 
 export const getPostsById = (postId) => baseApi.get(`/posts/postId=${postId}?_expand=user`);
 
-export const getPostsSearch = (search, page) => baseApi.get(`/posts?q=${search}&_page=${page}&limit=10&_expand=user`);
+export const getPostsSearch = (search, page) => baseApi.get(`/posts?q=${search}&_page=${page}&_limit=10&_expand=user`);
 
 export default {
 	getPosts,
